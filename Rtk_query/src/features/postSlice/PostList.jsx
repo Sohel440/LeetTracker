8593da08cc55eab1b528fab5dc43ffb33076e6e4
Sohel440 <codewithsohel2003@gmail.com>
2025-07-26 12:42:03 +0530

    Dark mode Light Mode fix

--- Rtk_query/src/features/postSlice/PostList.jsx
+++ Rtk_query/src/features/postSlice/PostList.jsx
@@ -49,8 +49,8 @@ export default function PostsList() {
           <span className="loading loading-bars loading-lg text-blue-600"></span>
           <span className="loading loading-bars loading-xl text-blue-600"></span>
         </div>
-      ) : (data) ? (
-        <div className="p-6 rounded-lg bg-[#1b2025] shadow-lg flex flex-col gap-6 items-center max-w-2xl w-full">
+      ) : (data || !data ) ? (
+        <div className="p-6 rounded-lg  bg-gradient-to-tr from-gray-700 to-gray-800 shadow-lg flex flex-col gap-6 items-center max-w-2xl w-full  ">
           <div className="text-center">
             <p className=" text-4xl  font-extrabold p-3 bg-clip-text text-transparent bg-gradient-to-r from-white to-blue-500">
               Total <span className="">Number of<br />Questions</span> Solved: <span className="bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-pink-500">{data ? data.totalSolved : "1000"}</span>
@@ -58,25 +58,25 @@ export default function PostsList() {
           </div>
           <div className="grid grid-cols-1 border-t-2 border-purple-600 border-dashed p-3 md:grid-cols-2 gap-6 font-bold font-mono">
             <p className="text-xl">
-              <span className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-green-600 to-white p-2 font-mono">Easy</span>: <span>{data ? `${data.easySolved} out of ${data.totalEasy}` : "400 out of 600"}</span> 
+              <span className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-green-600 to-white p-2 font-mono">Easy</span>: <span className=" light: text-white">{data ? `${data.easySolved} out of ${data.totalEasy}` : "400 out of 600"}</span> 
             </p>
             <p className="text-xl">
-              <span className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-amber-500 to-white p-2 font-mono">Medium</span>: <span>{data ? `${data.mediumSolved} out of ${data.totalMedium}` : "500 out of 1200"}</span>
+              <span className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-amber-500 to-white p-2 font-mono">Medium</span>: <span className=" light: text-white">{data ? `${data.mediumSolved} out of ${data.totalMedium}` : "500 out of 1200"}</span>
             </p>
             <p className="text-xl">
-              <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-800 to-white p-2 font-mono">Hard</span>: <span> {data ? `${data.hardSolved} out of ${data.totalHard}` : "100 out of 300"}</span>
+              <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-800 to-white p-2 font-mono">Hard</span>: <span className=" light: text-white"> {data ? `${data.hardSolved} out of ${data.totalHard}` : "100 out of 300"}</span>
             </p>
             <p className="text-xl">
-              <span className="font-bold text-[#7471f7] p-1 font-mono">Acceptance Rate</span>: <span>{data ? data.acceptanceRate : "85%"}</span> 
+              <span className="font-bold text-[#7471f7] p-1 font-mono">Acceptance Rate</span>: <span className=" light: text-white">{data ? data.acceptanceRate : "85%"}</span> 
             </p>
             <p className="text-xl">
-              <span className="font-bold text-[#7471f7] p-1 font-mono">Ranking</span>: <span>{data ? data.ranking : "12,345"}</span> 
+              <span className="font-bold text-[#7471f7] p-1 font-mono">Ranking</span>: <span className=" light: text-white">{data ? data.ranking : "12,345"}</span> 
             </p>
             <p className="text-xl">
-              <span className="font-bold text-[#7471f7] p-1 font-mono">Contribution Points</span>: <span>{data ? data.contributionPoints : "250"}</span> 
+              <span className="font-bold text-[#7471f7] p-1 font-mono">Contribution Points</span><span className=" light: text-white">: {data ? data.contributionPoints : "250"}</span> 
             </p>
             <p className="text-xl">
-              <span className="font-bold text-[#7471f7] p-1 font-mono">Likes</span>: <span>{data ? data.reputation : "50"}</span> 
+              <span className="font-bold text-[#7471f7] p-1 font-mono">Likes</span>: <span className=" light: text-white">{data ? data.reputation : "50"}</span> 
             </p>
           </div>
         </div>
